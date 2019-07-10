79da5d1f8c728a1c8b36c72ded12aadf000a579b
Vincent P. Minde <vincentminde@gmail.com>
2019-07-10 14:34:49 +0300

    Changed messaging on submit

--- routes/helpers/dataCollection.js
+++ routes/helpers/dataCollection.js
@@ -219,8 +219,8 @@ const completeForm = async (sessionid, phoneNumber) => {
   const phoneNumbers = [phoneNumber];
   //phoneNumbers.push();
   const orgUnitDetails = await getOrganisationUnit(orgUnit);
-  const message = 'Your Epidemiologic Week Number ' + period+ ' of ' + year+ 
-  ' IDSR Report has been successfully submitted with ID number ' +
+  const message = 'Taarifa ya mwaka ' + year + ' mwezi ' + period+ 
+  ' Kutoka FIN imepokelewa kikamilifu.' +
     + year + '' + period.substr(1) + '-' + (new Date()).toISOString().substr(14).split('.').join('').split(':').join('').split('Z').join('')
   + ', District: ' + orgUnitDetails.parent.name + ', Facility Name: ' + orgUnitDetails.name + '. Thank you';
   const result = await sendSMS(phoneNumbers, message);
