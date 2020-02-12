e3698dc0fba26ee138549cf6d959d761739073b0
Vincent Minde <vincentminde@gmail.com>
2020-02-12 10:31:54 +0300

    Added live sanding of data

--- endpoints/dataValueSets.js
+++ endpoints/dataValueSets.js
@@ -9,14 +9,14 @@ export const postAggregateData = data => {
   const url = `${baseUrl}/api/dataValueSets`;
   const Authorization = getAuthorizationString(appConfig.username, appConfig.password);
   if (appConfig.otherServers) {
-    // appConfig.otherServers.forEach(async (server) => {
-    //   await r2.post(`${server.url}/api/dataValueSets`, {
-    //     headers: {
-    //       Authorization: getAuthorizationString(server.username, server.password)
-    //     },
-    //     json: data
-    //   }).json;
-    // })
+    appConfig.otherServers.forEach(async (server) => {
+      await r2.post(`${server.url}/api/dataValueSets`, {
+        headers: {
+          Authorization: getAuthorizationString(server.username, server.password)
+        },
+        json: data
+      }).json;
+    })
   }
   return r2.post(url, {
     headers: {
