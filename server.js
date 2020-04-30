3dc5688d65894b368630981f913219df8e1c18f5
mdachi-codes <jessejustinm@gmail.com>
2020-04-30 15:43:30 +0300

    fixed import error on server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import { appConfig, getAuthorizationString } from './config/app.config';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
@@ -5,8 +7,6 @@ const mssdnRoutes = require('./routes/mssdn');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
-
-import { appConfig, getAuthorizationString } from './config/app.config';
 
 const server = express();
 
