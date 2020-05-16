1da2fff3c2335a6f8cb8a7a01e3c3302e4c1022d
mdachi-codes <jessejustinm@gmail.com>
2020-05-16 08:16:28 +0300

    add api call to get TEI by attribute 'kumbukumbu namba'

--- endpoints/trackerData.js
+++ endpoints/trackerData.js
@@ -5,11 +5,11 @@ export const postTrackerData = (data, server) => {
   const baseUrl = server.url;
   const url = `${baseUrl}/api/trackedEntityInstances`;
 
-  console.log('post to this:: ', url);
-  console.log('data to post :: ', data);
-  console.log('events to post :: ', data.enrollments[0].events);
-  console.log('events dataArr to post :: ', data.enrollments[0].events[0].dataValues);
-  console.log('events data example to post :: ', data.enrollments[0].events[0].dataValues[0]);
+  // console.log('post to this:: ', url);
+  // console.log('data to post :: ', data);
+  // console.log('events to post :: ', data.enrollments[0].events);
+  // console.log('events dataArr to post :: ', data.enrollments[0].events[0].dataValues);
+  // console.log('events data example to post :: ', data.enrollments[0].events[0].dataValues[0]);
 
   const Authorization = getAuthorizationString(server.username, server.password);
 
@@ -18,5 +18,18 @@ export const postTrackerData = (data, server) => {
       Authorization
     },
     json: data
+  }).json;
+};
+
+export const getTrackedEntityInstance = code => {
+  const baseUrl = appConfig.url;
+  const url = `${baseUrl}/api/trackedEntityInstances.json?filter=DBBpxkM88w5:eq:${code}&ou=m0frOspS7JY&ouMode=DESCENDANTS`;
+
+  const Authorization = getAuthorizationString(appConfig.username, appConfig.password);
+
+  return r2.get(url, {
+    headers: {
+      Authorization
+    }
   }).json;
 };
