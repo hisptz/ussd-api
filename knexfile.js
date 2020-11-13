93642359d746ce8c145daca10972d01653f67e4d
Vincent Minde <vincentminde@gmail.com>
2020-11-13 12:22:32 +0300

    Fixed database connection

--- knexfile.js
+++ knexfile.js
@@ -29,8 +29,8 @@ module.exports = {
     connection: {
       host:'database',
       database: 'ussd',
-      user: 'dhis',
-      password: 'dhis'
+      user: 'postgres',
+      password: 'postgres'
     },
     pool: {
       min: 2,
