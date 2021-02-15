973313d14dbd0b9cf607085c28a32b648220fa50
Jesse-Justin Mdachi <jessejustinm@gmail.com>
2021-02-15 16:41:14 +0300

    safe commit

--- routes/helpers/authentication.js
+++ routes/helpers/authentication.js
@@ -12,24 +12,44 @@ const {
 } = require('dhis2-uid');
 
 export const returnAuthenticationResponse = async (mssdn, sessionid) => {
+
+  console.log("napita humu ?? ")
+
   let response;
   const {
     users
   } = await getUserFromDHIS2(mssdn);
+
+  console.log('getUserResp :: ', users)
+
   const dataStore = await getDataStoreFromDHIS2();
+
+  console.log("datastore resp :: ", dataStore.settings)
+
   const {
     settings,
     menus
   } = dataStore;
+
   response = `C;${sessionid};${settings.no_user_message}`;
-  if (users.length) {
+  
+  if (users && users.length) {
+
+    console.log(1)
     const starting_menu = menus[settings.starting_menu];
     const name = users[0].displayName;
     const orgUnits = users[0].organisationUnits;
     response = `P;${sessionid};${`Welcome ${name} to Afya Reporting -- Enter PIN`}`;
     if (users.length > 1) {
+
+      console.log(2)
       response = `C;${sessionid};This phone number is associated with more than one user`;
+
+      console.log(3)
     } else {
+      
+      console.log(4)
+
       const id = generateCode();
       const session_data = {
         id,
@@ -42,12 +62,17 @@ export const returnAuthenticationResponse = async (mssdn, sessionid) => {
         done: false,
         retries: 0
       };
-      await addUserSession({
+
+      let userAdd = await addUserSession({
         ...session_data,
         //datastore: JSON.stringify(dataStore)
         datastore: dataStore
       });
+
+      console.log(5, " : ", userAdd)
     }
   }
+
+  console.log(6, " : ",response)
   return response;
 };
--- routes/mssdn.js
+++ routes/mssdn.js
@@ -19,7 +19,10 @@ const router = express.Router();
 
 const requestHandler = async (req, res) => {
   console.log('Called Port:', process.env.PORT);
+
+  console.log(1)
   if (appConfig.setSessionTimeout){
+    console.log(2)
     try {
       let {
         sessionid,
@@ -32,25 +35,31 @@ const requestHandler = async (req, res) => {
       let response;
       const session = await db.getCurrentSessionByPhoneNumber(msisdn, appConfig.setSessionTimeout);
       if (session) {
+        console.log(3)
         await db.updateUserSession(session.sessionid, {
           sessionid: sessionid
         });
         response = await repeatingRequest(sessionid, USSDRequest, msisdn);
       } else {
+        console.log(4)
+        console.log("kule")
         response = await returnAuthenticationResponse(msisdn, sessionid);
       }
       if (response.indexOf('C;') > -1) {
+        console.log(5)
         await db.updateUserSession(sessionid, {
           done: true
         });
       }
       res.send(response);
     } catch (e) {
+      console.log(6)
       res.send('C;${sessionid};Server Error. Please try again.');
       console.log(e.stack);
 
     }
   }else{
+    console.log(7)
     const {
       sessionid,
       telco,
@@ -62,10 +71,17 @@ const requestHandler = async (req, res) => {
     const isNewRequest = USSDType === 'NR';
     let response;
     if (isNewRequest) {
+      console.log(8)
+      console.log("huku")
+
       response = await returnAuthenticationResponse(msisdn, sessionid);
+      console.log("resp :: ", response)
     } else {
+      console.log(9)
       response = await repeatingRequest(sessionid, USSDRequest, msisdn);
     }
+
+    console.log(10)
     res.send(response);
   }  
 };
